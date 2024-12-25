fda31cc9866af4ad5ce5edeed780e5cf7dd1db8e
Roman Manzhula <roman1985al@ukr.net>
2024-12-25 18:17:59 +0200

    Update Main.java
    In Ukrainian

--- src/Main.java
+++ src/Main.java
@@ -6,66 +6,67 @@ public class Main {
     public static void main(String[] args) {
 
     }
-    public static Planets thePlanet; //создаем ссылочную переменную типа интерфейса Planets
+    public static Planets thePlanet; // створюємо посилальну змінну типу інтерфейсу Planets
 
-    static { //создаем статический блок, для запуска статического метода инициализации статического поля thePlanet
-        try { //пытаемся выполнить следующее
-            readTextFromConsoleAndInitPlanet(); //выполняем метод для инициализации планеты по введенной пользователем строке
-        } catch (IOException ioException) { //ловим исключение входных/выходных потоков
-            ioException.printStackTrace(); //выводим стек для пойманного исключения
+    static { // створюємо статичний блок, для запуску статичного методу ініціалізації статичного поля thePlanet
+        try { // намагаємось виконати наступне
+            readTextFromConsoleAndInitPlanet(); // виконуємо метод для ініціалізації планети за введеною користувачем рядком
+        } catch (IOException ioException) { // ловимо виключення для входу/виходу
+            ioException.printStackTrace(); // виводимо стек для зловленого виключення
         }
     }
 
-    public static void readTextFromConsoleAndInitPlanet() throws IOException { //метод для инициализации планеты по введенной пользователем строке
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); //открываем буферизированный
-        //поток для ввода данных с клавиатуры
+    public static void readTextFromConsoleAndInitPlanet() throws IOException { // метод для ініціалізації планети за введеною користувачем рядком
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in)); // відкриваємо буферизований
+        // потік для вводу даних з клавіатури
 
-        String inputString = bufferedReader.readLine().toLowerCase(); //создаем ссылочную переменную строкового типа, которую
-        // инициализируем строкой введенной пользователем и преобразованной в нижний регистр
+        String inputString = bufferedReader.readLine().toLowerCase(); // створюємо посилальну змінну строкового типу, яку
+        // ініціалізуємо рядком, введеним користувачем, і перетвореним на нижній регістр
 
-        if (inputString.equals("sun")) { //если введенная пользователем строка будет равна литералу "sun", то
-            thePlanet = Sun.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Sun
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("earth")) { //иначе если введенная пользователем строка будет равна литералу "earth", то
-            thePlanet = Earth.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Earth
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("moon")) { //иначе если введенная пользователем строка будет равна литералу "moon", то
-            thePlanet = Moon.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Moon
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("jupiter")) { //иначе если введенная пользователем строка будет равна литералу "jupiter", то
-            thePlanet = Jupiter.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Jupiter
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("mars")) { //иначе если введенная пользователем строка будет равна литералу "mars", то
-            thePlanet = Jupiter.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Mars
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("mercury")) { //иначе если введенная пользователем строка будет равна литералу "mercury", то
-            thePlanet = Jupiter.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Mercury
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("neptune")) { //иначе если введенная пользователем строка будет равна литералу "neptune", то
-            thePlanet = Jupiter.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Neptune
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("saturn")) { //иначе если введенная пользователем строка будет равна литералу "saturn", то
-            thePlanet = Jupiter.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Saturn
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("uranus")) { //иначе если введенная пользователем строка будет равна литералу "uranus", то
-            thePlanet = Jupiter.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Uranus
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else if (inputString.equals("venus")) { //иначе если введенная пользователем строка будет равна литералу "venus", то
-            thePlanet = Jupiter.getInstance(); //ссылочной переменной thePlanet присваиваем значение = Синглетону класса Venus
-            System.out.println(thePlanet.getClass()); //возвращаем название класса
-        } else { //иначе
-            thePlanet = null; //ссылочной переменной thePlanet присваиваем значение = null (остается пустой)
-            System.out.println("Not class"); //возвращаем нет класса
+        if (inputString.equals("sun")) { // якщо введений користувачем рядок буде рівний літералу "sun", то
+            thePlanet = Sun.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Sun
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("earth")) { // якщо введений користувачем рядок буде рівний літералу "earth", то
+            thePlanet = Earth.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Earth
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("moon")) { // якщо введений користувачем рядок буде рівний літералу "moon", то
+            thePlanet = Moon.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Moon
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("jupiter")) { // якщо введений користувачем рядок буде рівний літералу "jupiter", то
+            thePlanet = Jupiter.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Jupiter
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("mars")) { // якщо введений користувачем рядок буде рівний літералу "mars", то
+            thePlanet = Jupiter.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Mars
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("mercury")) { // якщо введений користувачем рядок буде рівний літералу "mercury", то
+            thePlanet = Jupiter.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Mercury
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("neptune")) { // якщо введений користувачем рядок буде рівний літералу "neptune", то
+            thePlanet = Jupiter.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Neptune
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("saturn")) { // якщо введений користувачем рядок буде рівний літералу "saturn", то
+            thePlanet = Jupiter.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Saturn
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("uranus")) { // якщо введений користувачем рядок буде рівний літералу "uranus", то
+            thePlanet = Jupiter.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Uranus
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else if (inputString.equals("venus")) { // якщо введений користувачем рядок буде рівний літералу "venus", то
+            thePlanet = Jupiter.getInstance(); // посилальній змінній thePlanet присвоюємо значення = синглетону класу Venus
+            System.out.println(thePlanet.getClass()); // повертаємо назву класу
+        } else { // в іншому випадку
+            thePlanet = null; // посилальній змінній thePlanet присвоюємо значення = null (залишається порожнім)
+            System.out.println("Not class"); // повертаємо "немає класу"
         }
     }
 }
 
-//Для решения этой задачи тебе нужно:
-//Найти пример паттерна Singleton с ленивой реализацией (lazy initialization).
-//По образу и подобию в отдельных файлах создать в отдельных файлах три класса синглтона — Sun, Moon, Earth....
-//Реализовать интерфейс Planets в классах Sun, Moon, Earth....
-//В статическом блоке класса Solution вызвать метод readKeyFromConsoleAndInitPlanet().
-//Реализовать функционал метода readKeyFromConsoleAndInitPlanet():
-//для этого считай с консоли один параметр типа String.
-//если он равен одной из констант интерфейса Planets, создай соответствующий объект и присвой его Planets thePlanet,
-// иначе — обнули Planets thePlanet.
+
+//Для вирішення цього завдання тобі потрібно:
+//Знайти приклад патерну Singleton з лінивою реалізацією (lazy initialization).
+/ / За образом і подобою в окремих файлах створити в окремих файлах три класи синглтона - Sun, Moon, Earth.
+//Реалізувати інтерфейс Planets у класах Sun, Moon, Earth.
+//У статичному блоці класу Solution викликати метод readKeyFromConsoleAndInitPlanet().
+//Реалізувати функціонал методу readKeyFromConsoleAndInitPlanet():
+//для цього рахуй з консолі один параметр типу String.
+//якщо він дорівнює одній з констант інтерфейсу Planets, створи відповідний об'єкт і присвойте його Planets thePlanet,
+/ / Інакше - обнули Planets thePlanet.
